656a923623718e286ff88629d019a87da0677ff8
gabrielrowan <gabrielrowan82@gmail.com>
2025-02-05 16:50:44 +0000

    search now responsive

--- booknook/library/forms.py
+++ booknook/library/forms.py
@@ -1,4 +1,7 @@
 from django import forms 
 
 class BookTitleFilterForm(forms.Form):
-    searchQuery = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Search by book or author', 'style': 'width: 400px;', 'class': 'form-control mx-2'}))
+    searchQuery = forms.CharField(required=False, 
+                                  widget=forms.TextInput(attrs={'placeholder': 'Search by book or author', 
+                                                                'style': 'width: clamp(250px, 30vw, 400px)', 
+                                                                'class': 'form-control mx-2'}))
