bacc7ed4a8d257928696fe902ffa51ea830a6fd5
gabrielrowan <gabrielrowan82@gmail.com>
2025-03-02 16:55:36 +0000

    added lots of login template forms

--- booknook/mysite/urls.py
+++ booknook/mysite/urls.py
@@ -22,4 +22,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')), 
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
