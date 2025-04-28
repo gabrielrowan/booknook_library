59d7c8ee31610d46cd04f7df069608baa5b389b6
gabrielrowan <gabrielrowan82@gmail.com>
2025-04-28 17:20:52 +0100

    added rate and review form to forms.py

--- booknook/library/forms.py
+++ booknook/library/forms.py
@@ -7,3 +7,14 @@ class BookTitleFilterForm(forms.Form):
                                                                 'style': 'width: clamp(250px, 30vw, 400px)', 
                                                                 'class': 'form-control mx-2'}))
     
+class RateAndReviewForm(forms.Form):
+    rating = forms.IntegerField(min_value=0, 
+                                max_value=5, 
+                                widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        help_text="Rate the book from 0 to 5.")
+    review = forms.CharField(
+        widget=forms.Textarea(max_length=2000,  
+                            help_text="Share your thoughts about the book.",
+                            attrs={'rows': 5, 'placeholder': 'Write your review here...', 
+                                     'class': 'form-control'})
+    )
