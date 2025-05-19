24297b6109937a800232e77f15b03b50e693844b
gabrielrowan <gabrielrowan82@gmail.com>
2025-05-19 10:34:29 +0100

    made subgenre container wrap for mobile

---
+++ booknook/staticfiles/js/script.29813e2a576e.js
@@ -0,0 +1,46 @@
+const stars = document.querySelectorAll('.star');
+const ratingInput = document.querySelector('input[name="rating"]');
+
+stars.forEach((star, idx) =>
+{
+    star.addEventListener('click', () =>
+    {
+        resetStars();
+        fillStars(idx);
+        ratingInput.value = idx + 1;
+    });
+
+    star.addEventListener('mouseenter', () =>
+    {
+        resetStars();
+        fillStars(idx);
+    });
+
+    star.addEventListener('mouseleave', () =>
+    {
+        resetStars();
+        const selectedRating = parseInt(ratingInput.value);
+        if (selectedRating)
+        {
+            fillStars(selectedRating - 1);
+        }
+    });
+});
+
+const resetStars = () =>
+{
+    stars.forEach(star =>
+    {
+        star.innerHTML = '&#9734;'; // empty star
+        star.classList.remove('star-selected');
+    });
+}
+
+const fillStars = (idx) =>
+{
+    for (let i = 0; i <= idx; i++)
+    {
+        stars[i].innerHTML = '&#9733;'; // filled star
+        stars[i].classList.add('star-selected');
+    }
+}
